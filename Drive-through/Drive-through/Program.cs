c0e8e7ddf58364dbb6d4542ed18e0d8c2f3c9dfe
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-01-06 17:15:38 +0300

    Added the Login Page

--- Drive-through/Drive-through/Program.cs
+++ Drive-through/Drive-through/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DS_Project_Trial;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Drive_through
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminMainPage());
+            Application.Run(new LoginPage());
         }
     }
 }
